62a09844fed80c002cb5f8cf2c8945a5c22fd8ac
Liina <165931823+LiinaA-E@users.noreply.github.com>
2024-04-06 14:43:19 +0300

    Update 06_04_24.py

--- 06_04_24.py
+++ 06_04_24.py
@@ -1,7 +1,7 @@
-Create a Python program for temperature conversion between Celsius and Fahrenheit scales.
-The program should prompt users to select the direction of conversion,
-accept input for the temperature value, and accurately perform the conversion (use IF and/or ELSE).
-Output should be displayed with formatted strings showing both the original and converted temperatures.
+#Create a Python program for temperature conversion between Celsius and Fahrenheit scales.
+#The program should prompt users to select the direction of conversion,
+#accept input for the temperature value, and accurately perform the conversion (use IF and/or ELSE).
+#Output should be displayed with formatted strings showing both the original and converted temperatures.
 
 ```py
 try:
